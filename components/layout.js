6ac47a642c6c3a680b682352ef01354b99e5ab9d
champdecay <dhruvangg@gmail.com>
2022-03-25 19:35:04 +0530

    Setup

---
+++ components/layout.js
@@ -0,0 +1,15 @@
+import Footer from "./Footer"
+import Header from "./Header"
+
+export default function Layout({ children }) {
+    return (
+        <main className="bg-white dark:bg-gray-900">
+            <Header />
+            <div className="body-container">
+                {children}
+            </div>
+            <Footer />
+        </main>
+    )
+}
+
