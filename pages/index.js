fabf9bbd9424bcbb676fa4852d06310e61350461
champdecay <dhruvangg@gmail.com>
2022-05-10 15:23:29 +0530

    update

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,17 @@ import Date from '../components/Date'
 //   }
 // }
 
-//export default function Home({ allPostsData }) {
+export async function getServerSideProps() {
+  const posts = await fetch(`https://dhruvangg-com.vercel.app/api/post?limit=3`)
+  const postsData = await posts.json()
+  const allPostsData = postsData
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
 export default function Home({ allPostsData }) {
   return (
     <Layout>
@@ -82,7 +92,7 @@ export default function Home({ allPostsData }) {
 
         <h3>Recent Articles</h3>
 
-        {/* <ul>
+        <ul>
           {allPostsData.map(({ _id, name, createdAt, slug }) => (
             <li key={_id}>
               <Link href={`/blog/${slug}`}><a>{name}</a></Link>
@@ -92,7 +102,7 @@ export default function Home({ allPostsData }) {
               </small>
             </li>
           ))}
-        </ul> */}
+        </ul>
 
       </section>
     </Layout>
