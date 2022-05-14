003b01960bc97a528899595af8897218e69e1e8c
champdecay <dhruvangg@gmail.com>
2022-05-14 15:14:01 +0530

    Blog listing

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import ReactMarkdown from 'react-markdown'
+import Date from '../../components/Date'
 import Layout from '../../components/Layout'
 
 export async function getStaticPaths() {
@@ -22,16 +24,35 @@ export async function getStaticProps({ params }) {
 }
 
 export default function Post({ postData }) {
+    const { name, excerpt, content, createdAt, tags } = postData
     return <Layout>
         <Head>
-            <title>{postData.name}</title>
+            <title>{`${name} | Dhruvang's Blog`}</title>
+            <meta name="title" content={name} />
+            <meta name="description" content={content} />
+            <meta name="keywords" content={tags.join(",")} />
+            <meta name="robots" content="index, follow" />
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="language" content="English" />
+            <meta name="revisit-after" content="5 days" />
+            <meta name="author" content="Dhruvang Gajjar" />
+
+            <meta property="og:type" content="website" />
+            <meta property="og:url" content="https://metatags.io/" />
+            <meta property="og:title" content={name} />
+            <meta property="og:description" content={content} />
+            <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+
+            <meta property="twitter:card" content="summary_large_image" />
+            <meta property="twitter:url" content="https://metatags.io/" />
+            <meta property="twitter:title" content={name} />
+            <meta property="twitter:description" content={content} />
+            <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
         </Head>
-        <article>
-            <h1>{postData.name}</h1>
-            <div>
-                <Date dateString={postData.createdAt} />
-            </div>
-            <div dangerouslySetInnerHTML={{ __html: postData.content }} />
+        <article className='prose lg:prose-xl dark:prose-invert mx-auto mt-8'>
+            <h1>{name}</h1>
+            <Date dateString={createdAt} />
+            <ReactMarkdown>{content}</ReactMarkdown>
         </article>
     </Layout>
 }
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,18 +1,53 @@
-
 import Head from 'next/head'
+import Date from '../../components/Date'
 import Layout from '../../components/Layout'
 
-export default function Blog() {
 
+export async function getServerSideProps() {
+    const posts = await fetch(`${process.env.APP_URI}/api/post`)
+    const postsData = await posts.json()
+    const allPostsData = postsData
+    return {
+        props: {
+            title: 'Blog',
+            description: 'Blog',
+            allPostsData
+        },
+    }
+}
+
+export default function Blog({ title, description, allPostsData }) {
     return <Layout>
         <Head>
-            <title>Blogs</title>
+            <title>{title}</title>
         </Head>
-        <main>
-            Hello World
-
-            <h3>Blog Listing Here</h3>
-        </main>
-    </Layout>
+        <main className='container mx-auto my-8'>
+            <section className="text-gray-600 body-font">
+                <div className="flex flex-wrap -m-4">
+                    {allPostsData && allPostsData.map(post => {
+                        const { _id, name, excerpt, slug, tags, createdAt } = post
+                        return <div key={_id} className="p-4 lg:w-1/3">
+                            <div className="h-full bg-gray-900 px-8 pt-10 pb-20 rounded-lg overflow-hidden text-center relative">
+                                <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-4">
+                                    {tags.map(tag => <span className="inline-block bg-gray-100 text-gray-900 rounded-full px-3 py-1 text-xs font-semibold mr-2">{tag}</span>)}
+                                </h2>
+                                <h1 className="title-font sm:text-2xl text-xl font-medium text-white mb-3">{name}</h1>
+                                <p className="leading-relaxed mb-3 text-gray-300">{excerpt}</p>
+                                <a className="text-indigo-400 inline-flex items-center" href={`/blog/${slug}`}>Learn More
+                                    <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                                        <path d="M5 12h14"></path>
+                                        <path d="M12 5l7 7-7 7"></path>
+                                    </svg>
+                                </a>
+                                <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-6 font-semibold text-sm text-white">
+                                    <Date dateString={createdAt} />
+                                </div>
+                            </div>
+                        </div>
+                    })}
+                </div>
+            </section>
+        </main >
+    </Layout >
 }
 
