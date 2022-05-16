315f681ccb0ff5f415a926ad61a5ce0db571aefc
champdecay <dhruvangg@gmail.com>
2022-05-16 13:20:07 +0530

    resolve blog listing

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Date from '../../components/Date'
 import Layout from '../../components/Layout'
+import Welcome from "../../public/images/welcome.png"
 
 
 export async function getServerSideProps() {
@@ -22,7 +23,7 @@ export default function Blog({ title, description, allPostsData }) {
             <title>{title}</title>
             <meta name="title" content={title} />
             <meta name="description" content={description} />
-            <meta name="keywords" content={tags.join(",")} />
+            <meta name="keywords" content="" />
             <meta name="robots" content="index, follow" />
             <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
             <meta name="language" content="English" />
@@ -49,7 +50,7 @@ export default function Blog({ title, description, allPostsData }) {
                         return <div key={_id} className="p-4 lg:w-1/3">
                             <div className="h-full bg-gray-900 px-8 pt-10 pb-20 rounded-lg overflow-hidden text-center relative">
                                 <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-4">
-                                    {tags.map(tag => <span className="inline-block bg-gray-100 text-gray-900 rounded-full px-3 py-1 text-xs font-semibold mr-2">{tag}</span>)}
+                                    {tags.map((tag, i) => <span key={i} className="inline-block bg-gray-100 text-gray-900 rounded-full px-3 py-1 text-xs font-semibold mr-2">{tag}</span>)}
                                 </h2>
                                 <h1 className="title-font sm:text-2xl text-xl font-medium text-white mb-3">{name}</h1>
                                 <p className="leading-relaxed mb-3 text-gray-300">{excerpt}</p>
